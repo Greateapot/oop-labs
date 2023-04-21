f9347e3d394e3b890134da49d0de3c0f940aa478
Greateapot <greateapot998@gmail.com>
2023-04-21 22:43:47 +0500

    lab11

--- lab11/src/main.cpp
+++ lab11/src/main.cpp
@@ -1,8 +1,346 @@
 #include <iostream>
+#include <random>
+#include <ctime>
+#include <list>
+#include <queue>
+
+#include "list.hpp"
+#include "pair/pair.hpp"
 
 using namespace std;
 
+const size_t SIZE = 10;
+
+template <class T>
+void print(list<T> &ls)
+{
+  for (T v : ls)
+    cout << v << ", ";
+  cout << endl;
+}
+
+template <class T>
+void print(priority_queue<T> &ls)
+{
+  priority_queue<T> temp;
+  while (!ls.empty())
+  {
+    cout << ls.top() << ", ";
+    temp.push(ls.top());
+    ls.pop();
+  }
+
+  cout << endl;
+
+  while (!temp.empty())
+  {
+    ls.push(ls.top());
+    temp.pop();
+  }
+}
+
+void ex1()
+{
+  list<double> ls;
+
+  for (int i = 0; i < SIZE; i++)
+    ls.push_back(rand() % 1000);
+
+  print(ls);
+
+  // 1
+  {
+    double sum = 0;
+    for (double e : ls)
+      sum += e;
+
+    ls.push_back(sum / ls.size());
+    print(ls);
+  }
+
+  // 2
+  {
+    list<double> temp = ls;
+    ls.clear();
+
+    double a, b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+
+    for (double e : temp)
+      if (e < a || e > b)
+        ls.push_back(e);
+
+    print(ls);
+  }
+
+  // 3
+  {
+    bool first = 1;
+    double min = *(ls.begin()), max = min;
+    for (double e : ls)
+      if (!first)
+      {
+        if (e < min)
+          min = e;
+        else if (e > max)
+          max = e;
+      }
+      else
+        first = 0;
+
+    const double sum = min + max;
+    cout << "Sum: " << sum << endl;
+
+    for (double &e : ls)
+      e += sum;
+
+    print(ls);
+  }
+}
+
+void ex2()
+{
+  list<Pair> ls;
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    const int v = rand() % 1000000;
+    ls.push_back(Pair(v / 1000, v % 1000 * 0.001));
+  }
+
+  print(ls);
+
+  // 1
+  {
+    Pair sum;
+    for (Pair e : ls)
+      sum += e;
+
+    ls.push_back(Pair(sum.get_a() / ls.size(), sum.get_b() / (double)ls.size()));
+    print(ls);
+  }
+
+  // 2
+  {
+    list<Pair> temp = ls;
+    ls.clear();
+
+    Pair a, b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+
+    for (Pair e : temp)
+      if (e < a || e > b)
+        ls.push_back(e);
+
+    print(ls);
+  }
+
+  // 3
+  {
+    bool first = 1;
+    Pair min = *(ls.begin()), max = min;
+    for (Pair e : ls)
+      if (!first)
+      {
+        if (e < min)
+          min = e;
+        else if (e > max)
+          max = e;
+      }
+      else
+        first = 0;
+
+    const Pair sum = min + max;
+    cout << "Sum: " << sum << endl;
+
+    for (Pair &e : ls)
+      e += sum;
+
+    print(ls);
+  }
+}
+
+void ex3()
+{
+  List<Pair> *ls = new List<Pair>();
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    const int v = rand() % 1000000;
+    ls->push_back(new Node<Pair>(Pair(v / 1000, v % 1000 * 0.001)));
+  }
+
+  ls->print();
+
+  // 1
+  {
+    Pair sum;
+    for (Iterator<Pair> it = ls->first(); it != ls->last(); it++)
+      sum += (*it).get_key();
+
+    ls->push_back(new Node<Pair>(Pair(sum.get_a() / ls->get_size(), sum.get_b() / (double)ls->get_size())));
+    ls->print();
+  }
+
+  // 2
+  {
+    Pair a, b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+
+    for (Iterator<Pair> it = ls->first(); it != ls->last(); it++)
+    {
+      Pair e = (*it).get_key();
+      if (e < a || e > b)
+        ls->queue_remove(e);
+    }
+
+    ls->print();
+  }
+
+  // 3
+  {
+    bool first = 1;
+    Pair min = (*(ls->first())).get_key(), max = min;
+    for (Iterator<Pair> it = ls->first(); it != ls->last(); it++)
+    {
+      Pair e = (*it).get_key();
+
+      if (!first)
+      {
+        if (e < min)
+          min = e;
+        else if (e > max)
+          max = e;
+      }
+      else
+        first = 0;
+    }
+
+    const Pair sum = min + max;
+    cout << "Sum: " << sum << endl;
+
+    for (Iterator<Pair> it = ls->first(); it != ls->last(); it++)
+      (*it).set_key((*it).get_key() + sum);
+
+    ls->print();
+  }
+}
+
+void ex4()
+{
+  priority_queue<Pair> ls;
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    const int v = rand() % 1000000;
+    ls.push(Pair(v / 1000, v % 1000 * 0.001));
+  }
+
+  print(ls);
+
+  // 1
+  {
+    priority_queue<Pair> temp;
+
+    Pair sum;
+    while (!ls.empty())
+    {
+      sum += ls.top();
+      temp.push(ls.top());
+      ls.pop();
+    }
+
+    while (!temp.empty())
+    {
+      ls.push(temp.top());
+      temp.pop();
+    }
+
+    ls.push(Pair(sum.get_a() / ls.size(), sum.get_b() / (double)ls.size()));
+    print(ls);
+  }
+
+  // 2
+  {
+    priority_queue<Pair> temp;
+
+    Pair a, b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+
+    while (!ls.empty())
+    {
+      if (Pair(ls.top()) < a || Pair(ls.top()) > b)
+        temp.push(ls.top());
+      ls.pop();
+    }
+
+    while (!temp.empty())
+    {
+      ls.push(temp.top());
+      temp.pop();
+    }
+
+    print(ls);
+  }
+
+  // 3
+  {
+
+    priority_queue<Pair> temp;
+    bool first = 1;
+    Pair min = ls.top(), max = min;
+
+    while (!ls.empty())
+    {
+      Pair e = ls.top();
+      if (!first)
+      {
+        if (e < min)
+          min = e;
+        else if (e > max)
+          max = e;
+      }
+      else
+        first = 0;
+
+      temp.push(e);
+      ls.pop();
+    }
+
+    const Pair sum = min + max;
+    cout << "Sum: " << sum << endl;
+
+    while (!temp.empty())
+    {
+      ls.push(Pair(temp.top()) + sum);
+      temp.pop();
+    }
+
+    print(ls);
+  }
+}
+
+void ex5() { cout << "Мне stl list скопировать надо, чтоб это выполнить?" << endl; }
+
 int main()
 {
+  srand((int)time(0));
+  ex1();
+  ex2();
+  ex3();
+  ex4();
+  ex5();
   return 0;
 }
---
+++ lab11/src/pair/pair.cpp
@@ -0,0 +1,178 @@
+#include "pair.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+using namespace std;
+
+Pair::Pair() : a(0), b(.0) {}
+Pair::Pair(const int a, const double b) : a(a), b(b) {}
+Pair::Pair(const Pair &other) : a(other.a), b(other.b) {}
+Pair::~Pair() {}
+
+void Pair::set_a(const int v) { a = v; }
+int Pair::get_a() const { return a; }
+
+void Pair::set_b(const double v) { b = v; }
+double Pair::get_b() const { return b; }
+
+
+
+Pair Pair::operator+(const int v)
+{
+    return Pair(a + v, b);
+}
+
+Pair Pair::operator+(const double v)
+{
+    return Pair(a, b + v);
+}
+
+Pair Pair::operator+(const Pair &other)
+{
+    return Pair(a + other.a, b + other.b);
+}
+
+Pair Pair::operator-(const Pair &other)
+{
+    return Pair(a - other.a, b - other.b);
+}
+
+
+Pair &Pair::operator=(const Pair &other)
+{
+    a = other.a;
+    b = other.b;
+    return *this;
+}
+
+Pair &Pair::operator+=(const int v)
+{
+    return (*this = (*this + v));
+}
+
+Pair &Pair::operator+=(const double v)
+{
+    return (*this = (*this + v));
+}
+
+Pair &Pair::operator+=(const Pair& other)
+{
+    return (*this = (*this + other));
+}
+
+Pair &Pair::operator-=(const Pair &other)
+{
+    return (*this = (*this - other));
+}
+
+bool Pair::operator>(const Pair &other)
+{
+    return this->a > other.a && this->b > other.b;
+}
+
+bool Pair::operator<(const Pair &other)
+{
+    return this->a < other.a && this->b < other.b;
+}
+
+bool Pair::operator==(const Pair &other)
+{
+    return this->a == other.a && this->b == other.b;
+}
+
+bool Pair::operator<=(const Pair &other)
+{
+    return (*this < other) || (*this == other);
+}
+
+bool Pair::operator>=(const Pair &other)
+{
+    return (*this > other) || (*this == other);
+}
+
+bool Pair::operator!=(const Pair &other)
+{
+    return !((*this) == other);
+}
+
+ostream &operator<<(ostream &out, const Pair &pair)
+{
+    return (out << pair.a << " : " << pair.b);
+}
+
+istream &operator>>(istream &in, Pair &pair)
+{
+    bool F = 1;
+    string buf("");
+
+    do
+    {
+        cout << "Input a: ";
+        in.sync();
+        getline(in, buf, ' ');
+
+        try
+        {
+            pair.a = stoi(buf);
+            F = 0;
+        }
+        catch (invalid_argument &)
+        {
+            cout << "Incorrect input!" << endl;
+        }
+    } while (F);
+
+    F = 1;
+    buf = "";
+
+    do
+    {
+        cout << "Input b: ";
+        in.sync();
+        getline(in, buf, ' ');
+
+        try
+        {
+            pair.b = stod(buf);
+            F = 0;
+        }
+        catch (invalid_argument &)
+        {
+            cout << "Incorrect input!" << endl;
+        }
+    } while (F);
+
+    return in;
+}
+
+ofstream &operator<<(ofstream &out, const Pair &pair)
+{
+    out.write((const char *)&(pair.a), sizeof(int));
+    out.write((const char *)&(pair.b), sizeof(double));
+    out.write(&PAIR_DELIMITER, 1);
+    return out;
+}
+
+ifstream &operator>>(ifstream &in, Pair &pair)
+{
+    try
+    {
+        char c;
+        in.read((char *)&(pair.a), sizeof(int));
+        in.read((char *)&(pair.b), sizeof(double));
+        in.read(&c, 1);
+    }
+    catch (...)
+    {
+        cout << "Incorrect input!" << endl;
+    }
+
+    return in;
+}
+
+bool operator<(const Pair &a, const Pair &b)
+{
+    return a.a < b.a && b.a < b.b;
+}
